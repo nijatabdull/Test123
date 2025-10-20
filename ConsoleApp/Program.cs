afe9c8025f42ee20a5a0e099bc66bcdf3530dc17
nijatabdull <nijattttsa@gmail.com>
2025-10-20 19:48:25 +0400

    Initial Commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        // Entry Point
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int value = random.Next(10, 20);
+        }
+    }
+}
